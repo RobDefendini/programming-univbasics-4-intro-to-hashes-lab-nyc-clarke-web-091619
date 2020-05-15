0d205132b277234ca78cf1c0efed71a93dcfb829
Robert Defendini <robdefendini@gmail.com>
2020-05-15 18:10:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,9 +33,8 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key]
-  hash["top key"] += 1
-else
-  puts "Key not found!"
+  hash["top key"key] += 1
+  
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
