4b9a85b379564895862ca05537ecc61a5b255a77
Robert Defendini <robdefendini@gmail.com>
2020-05-15 17:46:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,9 @@ id_generator = {
 end
 
 def my_hash_creator(key, value)
+  my_hash_creator = {
+    
+  }
  my_hash_creator[name:] = 'Grace Hopper'
 my_hash_creator
   # return a hash that includes the key and value parameters passed into this method
